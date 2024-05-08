b58f6f3bcbf814e8d153f89ffe457719134254b5
Diaa Mohamed <57022100+Diaamohamed29@users.noreply.github.com>
2024-05-08 11:04:50 +0300

    forth

--- HR/blueprints/admin/routes.py
+++ HR/blueprints/admin/routes.py
@@ -6,12 +6,30 @@ from datetime import datetime , timedelta
 admin = Blueprint("admin", __name__, template_folder="templates")
 
 
+
+
+### MAIN ADMIN PAGE
 @admin.route('/')
 def index():
     return render_template('admin/index.html')
+### END ADMIN PAGE ###
+
+
+### EMPLOYES PAGES ###
+
 
 @admin.route('/employes')
 def employes():
+
+    return render_template('admin/employes.html')
+
+
+@admin.route('/add_employe')
+def add_employe():
+    return render_template('admin/add_employe.html')
+
+@admin.route('/view_employes')
+def view_employes():
     cursor.execute(""" 
 
             SELECT COUNT(*) AS row_count
@@ -26,11 +44,18 @@ def employes():
                     qualification , national_id , address , telephone , hire_date ,
                    net_salary , allowance from employes """)
     results = cursor.fetchall()
-    return render_template('admin/employes.html',number=number,results=results)
+    return render_template('admin/view_employes.html',number=number,results=results)
+
+
+### END EMPLOYES PAGES ###
 
 
 
 
+
+
+
+### ATTENDANCE PAGE ###
 
 @admin.route('/submit_attendance',methods=['POST'])
 def submit_attendance():
@@ -69,11 +94,11 @@ def attendance():
     return render_template('admin/attendance.html',all_employes=all_employes)
 
 
+### END ATTENDANCE PAGE ###
 
 
 
-
-
+### VACATIONS PAGE ###
 @admin.route('/vacations')
 def vacations():
     return render_template('admin/vacations.html')
@@ -83,9 +108,10 @@ def all_vacations():
 @admin.route('/add_vacation')
 def add_vacation():
     return render_template('admin/add_vacation.html')
+### END VACATIONS PAGE ###
 
 
-
+### MISSIONS PAGE ###
 
 @admin.route('/missions')
 def missions():
@@ -96,3 +122,30 @@ def add_mission():
 @admin.route('/view_missions')
 def view_missions():
     return render_template('admin/view_missions.html')
+
+### END MISSIONS PAGE ###
+
+
+
+
+### REPORTS PAGES ###
+@admin.route('/reports')
+def reports():
+    return render_template('admin/reports.html')
+### END REPORT PAGE ###
+
+
+### GAZ PAGE ###
+@admin.route('/gaz')
+def gaz():
+    return render_template('admin/gaz.html')
+### END GAZ PAGE ###
+
+
+
+
+### SALARIES PAGE ###
+@admin.route('/salaries')
+def salaries():
+    return render_template('admin/salaries.html')
+### END SALARIES PAGE ###
